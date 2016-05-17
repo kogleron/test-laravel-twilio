426e90bc66ec0c92f308ea616dc2c19d42c3f1fe
kogleron <kogleron@gmail.com>
2016-05-17 19:39:22 +0300

    Functional test with a sequence of API requests

--- tests/PageTest.php
+++ tests/PageTest.php
@@ -2,8 +2,94 @@
 
 class PageTest extends TestCase
 {
+    protected function getPhone()
+    {
+        $mockPhoneNumber               = Mockery::mock();
+        $mockPhoneNumber->phone_number = '+12345';
+
+        $mockLookupPhoneNumber               = Mockery::mock();
+        $mockLookupPhoneNumber->country_code = 'us';
+
+        $mockTwilioService                                  = Mockery::mock();
+        $mockTwilioService->account                         = Mockery::mock();
+        $mockTwilioService->account->incoming_phone_numbers = [$mockPhoneNumber];
+
+        $mockTwilioLookupsService                = Mockery::mock();
+        $mockTwilioLookupsService->phone_numbers = Mockery::mock();
+        $mockTwilioLookupsService->phone_numbers->shouldReceive('get')
+            ->andReturn($mockLookupPhoneNumber);
+
+        App::instance('Twilio', $mockTwilioService);
+        App::instance('TwilioLookups', $mockTwilioLookupsService);
+
+        /**
+         * Existing number
+         */
+        $response = $this->call('GET', 'api/phone/us');
+
+        $this->assertJsonStringEqualsJsonString(json_encode('+12345'), $response->getContent());
+
+        /**
+         * Buy number
+         */
+
+        $mockPhoneNumber->phone_number = '+76543';
+
+        $mockAvailablePhoneNumbers                          = Mockery::mock();
+        $mockAvailablePhoneNumbers->available_phone_numbers = [$mockPhoneNumber];
+
+        $mockTwilioService->account->incoming_phone_numbers = Mockery::mock();
+        $mockTwilioService->account->incoming_phone_numbers->shouldReceive('create');
+
+        $mockTwilioService->account->available_phone_numbers = Mockery::mock();
+        $mockTwilioService->account->available_phone_numbers->shouldReceive('getList')
+            ->andReturn($mockAvailablePhoneNumbers);
+
+        $response = $this->call('GET', 'api/phone/us');
+
+        $this->assertJsonStringEqualsJsonString(json_encode('+76543'), $response->getContent());
+    }
+
+    protected function getCountries()
+    {
+        $mockTwilioService = Mockery::mock();
+
+        $mockAvailablePhoneNumbers                          = Mockery::mock();
+        $mockAvailablePhoneNumbers->available_phone_numbers = [1];
+
+        $mockTwilioService->account                          = Mockery::mock();
+        $mockTwilioService->account->available_phone_numbers = Mockery::mock();
+        $mockTwilioService->account->available_phone_numbers->shouldReceive('getList')->andReturn($mockAvailablePhoneNumbers);
+
+        $mockTwilioPricingService = Mockery::mock();
+
+        $mockCountryGB              = Mockery::mock();
+        $mockCountryGB->iso_country = 'gb';
+
+        $mockCountryCA              = Mockery::mock();
+        $mockCountryCA->iso_country = 'ca';
+
+        $mockTwilioPricingService->phoneNumberCountries = [
+            $mockCountryGB,
+            $mockCountryCA
+        ];
+
+        App::instance('Twilio', $mockTwilioService);
+        App::instance('TwilioPricing', $mockTwilioPricingService);
+
+        $response = $this->call('GET', 'api/countries');
+
+        $this->assertJson($response->getContent());
+
+        $responseCountries = json_decode($response->getContent());
+
+        sort($responseCountries);
+
+        $this->assertEquals(['ca', 'gb', 'us'], $responseCountries);
+    }
+
     /**
-     * A basic functional test example.
+     * A functional test
      *
      * @return void
      */
@@ -11,5 +97,15 @@ class PageTest extends TestCase
     {
         $this->visit('/')
             ->see('Twilio');
+
+        /*
+         * Ajax request
+         */
+        $this->getCountries();
+
+        /*
+         * Ajax request after clicking on a country
+         */
+        $this->getPhone();
     }
 }
